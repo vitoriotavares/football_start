4e558903ac1ce825af238c36ae804eeb6fc1b2ef
Vitorio <norbdus@gmail.com>
2024-10-29 13:38:55 -0300

    Run configuration script.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter = :resque
-  # config.active_job.queue_name_prefix = "untitled_application_production"
+  # config.active_job.queue_name_prefix = "app_example_soccer_agency_production"
 
   # Disable caching for Action Mailer templates even if Action Controller
   # caching is enabled.
